9cd640af5e8155126e110c4bfd9b837f2c90466d
masfuego <75271386+masfuego@users.noreply.github.com>
2022-05-04 18:46:20 +0000

    added if statements to make a smarter calculator

--- main.dart
+++ main.dart
@@ -1,19 +1,31 @@
 import 'dart:math';
 import 'dart:io';
 
+String prompt(String promptText){
+  print("The prompt is: ${promptText}");
+  String answer = stdin.readLineSync()!;
+  return answer;
+}
+
+double promptDouble(){
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
 void main() {
 
-  bool isSmart = true;
-  bool isStudent = false;
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("enter an operation: ");
 
-  if(isSmart && isStudent){
-    print("you're a smart student");
-  } else if(isSmart && !isStudent){
-    print("you're smart but not a student");
-  } else if(!isSmart && isStudent){
-    print("you're not a smart student");
+  if(op == '+'){
+    print(num1 + num2);
+  } else if(op == '-'){
+    print(num1 - num2);
+  }  else if(op == '*'){
+    print(num1 * num2);
   } else {
-    print("you're neither");
+    print("invalid operator");
   }
-  
+
 }
