594a45f003d8957fc13661186a38d5fd28a54796
masfuego <75271386+masfuego@users.noreply.github.com>
2022-05-04 19:12:08 +0000

    created a while loop

--- main.dart
+++ main.dart
@@ -1,37 +1,11 @@
 import 'dart:math';
 import 'dart:io';
 
-String prompt(String promptText){
-  print("The prompt is: ${promptText}");
-  String answer = stdin.readLineSync()!;
-  return answer;
-}
+void main(){
 
-double promptDouble(){
-  print("Enter a number: ");
-  double myNum = double.parse(stdin.readLineSync()!);
-  return myNum;
-}
-void main() {
-
-  double num1 = promptDouble();
-  double num2 = promptDouble();
-  String op = prompt("enter an operation: ");
-
-  switch(op){
-    case '+': 
-      print(num1 + num2);
-      break;
-    case '-': 
-      print(num1 - num2);
-      break;
-    case '*': 
-      print(num1 * num2);
-      break;
-    case '/': 
-      print(num1 / num2);
-      break;
-    default:
-      print("invalid operator");
+  int i = 0;
+  while(i < 5){ //loop guard
+    print(i);
+    i++;
   }
 }
